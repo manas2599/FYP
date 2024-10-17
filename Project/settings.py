dc4cc5e91debb237dd5605223fc4ad0040fad812
Anas <manaskhan2599@gmail.com>
2024-10-17 08:49:57 +0500

    done vercel setting

--- Project/settings.py
+++ Project/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
